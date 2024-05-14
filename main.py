d0392341a98481b13e2167f845e79ebae827b692
fal <faisal_a@live.co.uk>
2024-05-14 13:48:27 +0100

    update for sqllite commands

--- main.py
+++ main.py
@@ -4,10 +4,14 @@
 
 from dataframe_processing import read_csv_and_process
 
+from sql_commands import create_db_table, get_results, get_result_by_id, insert_result
+
 from flask import Flask, request, jsonify
 import pandas as pd
 from werkzeug.utils import secure_filename
 import os
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
@@ -16,6 +20,31 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/results', methods=['GET'])
+def api_get_results():
+    return jsonify(())
+
+@app.route('/api/results/<result_id>', methods=['GET'])
+def api_get_result(result_id):
+    return jsonify(get_result_by_id(result_id))
+
+@app.route('/api/results/add',  methods = ['POST'])
+def api_add_results():
+    result = request.get_json()
+    print('result : ', result)
+    return jsonify(insert_result(result))
+
+# @app.route('/api/results/update',  methods = ['PUT'])
+# def api_update_user():
+#     result = request.get_json()
+#     return jsonify(update_result(user))
+#
+# @app.route('/api/users/delete/<user_id>',  methods = ['DELETE'])
+# def api_delete_user(user_id):
+#     return jsonify(delete_user(user_id))
 
 @app.route("/summary")
 def summary():
@@ -51,4 +80,10 @@ def upload_csv():
 
 
 if __name__ == '__main__':
+    create_db_table()
     app.run(debug=True)
+
+# if __name__ == "__main__":
+#     #app.debug = True
+#     #app.run(debug=True)
+#     app.run() #run app
---
+++ sql_commands.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import sqlite3
+
+
+def connect_to_db():
+    conn = sqlite3.connect('database.db')
+    return conn
+
+
+def create_db_table():
+    try:
+        conn = connect_to_db()
+        conn.execute('''
+            CREATE TABLE results (
+                result_id INTEGER PRIMARY KEY NOT NULL,
+                time_submitted TEXT NOT NULL
+            );
+        ''')
+
+        conn.commit()
+        print("results table created successfully")
+    except:
+        print("results table creation failed - Maybe table")
+    finally:
+        conn.close()
+
+
+def insert_result(result):
+    inserted_result = {}
+
+    try:
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute(
+            "INSERT INTO results (time_submitted)"
+            "VALUES (?)",
+            (result['time_submitted'],)
+        )
+
+        conn.commit()
+        inserted_result = get_result_by_id(cur.lastrowid)
+
+    except:
+        conn().rollback()
+
+    finally:
+        conn.close()
+
+    return inserted_result
+
+
+def get_results():
+    results = []
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM results")
+        rows = cur.fetchall()
+
+        # convert row objects to dictionary
+        for i in rows:
+            user = {}
+            user["result_id"] = i["result_id"]
+            user["time_submitted"] = i["time_submitted"]
+
+            results.append(user)
+
+    except:
+        results = []
+
+    return results
+
+def get_result_by_id(result_id):
+    result = {}
+
+    try:
+        conn = connect_to_db()
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM results WHERE result_id = ?",
+                       (result_id,))
+
+        row = cur.fetchone()
+
+        # convert row object to dictionary
+        result["result_id"] = row["result_id"]
+        result["time_submitted"] = row["time_submitted"]
+
+    except:
+        result = {}
+
+    return result
